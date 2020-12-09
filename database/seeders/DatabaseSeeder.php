db9dc649cae04414b7b5dc9a01e55c385d0fcb90
tridung240400@gmail.com <0383Springbbe2404>
2020-12-09 20:23:01 +0700

    remove seed post

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
             RoleSeeder::class,
             UserSeeder::class,
             RoomTypeSeeder::class,
-            PostSeeder::class,
+            //PostSeeder::class,
         ]);
     }
 }
