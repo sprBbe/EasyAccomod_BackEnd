4c48478918428c44bccb159bb190abcd8a576c28
SpringBbe <tridung240400@gmail.com>
2020-12-14 23:33:09 +0700

    post resource

---
+++ app/Http/Middleware/AdminCheck.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class AdminCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        //$x = $request->user();
+        if($request->user('api')){
+            $user = $request->user('api');
+            if($user->id_role==3){
+                return $next($request);
+            }
+            else{
+                return response()->json([
+                    'message' => 'Không có quyền quản trị'
+                ],403);
+            }
+        }
+        else{
+            return response()->json([
+                'message' => 'Chưa đăng nhập'
+            ],401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -31,10 +31,15 @@ Route::group(['namespace' => 'App\Http\Controllers'], function () {
     Route::get('get_all_provinces', 'PagesController@getAllProvinces')->name('get_all_provinces');
     Route::get('get_district_by_id_province/{id_province}', 'PagesController@getDistrictByIdProvince');
     Route::get('get_ward_by_id_district/{id_district}', 'PagesController@getWardByIdDistrict');
-    Route::get('filter','PagesController@getFilter');
+    Route::get('filter', 'PagesController@getFilter');
     Route::group(['middleware' => 'auth:api'], function () {
         Route::post('new_post', 'PagesController@postNewPost')->name('new_post');
         Route::post('edit_profile', 'PagesController@postEditProfile')->name('post_edit_profile');
         Route::post('change_password', 'PagesController@postChangePassword')->name('post_change_password');
     });
+});
+Route::group(['prefix' => 'admin', 'namespace' => 'App\Http\Controllers\AdminController', 'middleware' => 'admin.check'], function () {
+    Route::apiResources([
+        'posts' => 'PostController',
+    ]);
 });
